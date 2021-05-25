863ff2898b7e4e28bac60bfd01e59667f44bcb7f
Jerry Xiong <jerryxiong@Serenas-MacBook-Air.local>
2021-05-24 19:29:29 -0500

    made a connected POST route for client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,30 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+
+
+// Click Listeners
+
+// Create a POST route for with click listener
+$('#addJJokeButton').on('click', addjoke)
+
+} // END onReaedy
+
+
+function addjoke(){
+    let newJoke = {
+        whoseJoke:    $('#whoseJokeIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine:    $('#punchLineIn').val()
+    }
+
+    $.ajax({
+        url: '/items',
+        method: 'POST',
+        data: newJoke
+    }).then(reponse => {
+        console.log('should be 201', reponse);
+        //Rerun GET post to update DOM
+    })
 }
